8523c9473efc9d28c2b4dbe74efcc6fc2af5da26
Tessa <48088982+Tesax123@users.noreply.github.com>
2024-10-15 20:16:05 +0200

    Update a_favoriete_dieren.py (#1)

--- oplossingen/a_favoriete_dieren.py
+++ oplossingen/a_favoriete_dieren.py
@@ -1,16 +1,8 @@
-# Maak een programma dat:
-# - een lege lijst aanmaakt
-# - drie dieren vraagt aan de user
-# - deze dieren toe voegt aan de lijst
-# - de lijst met dieren toont aan de user
-
 favoriete_dieren = []
 
-def vraag_dier():
-    return input("Geef één van je favoriete dieren: ")
-
 for _ in range(3):
-    favoriete_dieren.append(vraag_dier())
+    dier = input("Geef één van je favoriete dieren: ")
+    favoriete_dieren.append(dier)
 
 print("Jouw favoriete dieren:")
 print(favoriete_dieren)
